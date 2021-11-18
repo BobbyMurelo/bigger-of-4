e773641f9088e6d43d7b479ab2e2a6244d5f2364
bobby murelo <murelobobby99@gmail.com>
2021-11-18 16:18:42 +0200

    Add project files.

---
+++ bigger of 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class TheBiggestOfFourNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("Please write 4 numbers:");
+        decimal a = decimal.Parse(Console.ReadLine());
+        decimal b = decimal.Parse(Console.ReadLine());
+        decimal c = decimal.Parse(Console.ReadLine());
+        decimal d = decimal.Parse(Console.ReadLine());
+        
+
+        if ((a >= b) && (a >= c) && (a >= d))
+        {
+            Console.WriteLine("The biggest number is: {0}", a);
+            return;
+        }
+        if ((b >= a) && (b >= c) && (b >= d) && (b >= c))
+        {
+            Console.WriteLine("The biggest number is: {0}", b);
+            return;
+        }
+        if ((c >= a) && (c >= b) && (c >= d))
+        {
+            Console.WriteLine("The biggest number is: {0}", c);
+            return;
+        }
+        if ((d >= a) && (d >= b) && (d >= c))
+        {
+            Console.WriteLine("The biggest number is: {0}", d);
+            return;
+        }
+     
+    }
+}
